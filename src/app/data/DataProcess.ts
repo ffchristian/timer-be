49602a579b314c7e4872301b4131cedb158aef7a
Christian Farias <christian.farias@mecuris.com>
2021-07-07 17:27:22 +0200

    add mongoose to change mongo approach, change env vars names

--- src/app/data/DataProcess.ts
+++ src/app/data/DataProcess.ts
@@ -9,9 +9,9 @@ export class DataProcess {
   }
 
   public async connect(): Promise<any> {
-    const connectionString: string = process.env.mongodb_app || "";
-    const mongoClient = await MongoClient.connect(connectionString);
-    this.database = mongoClient.db(this.databaseName);
+    // const connectionString: string = process.env.mongodb_app || "";
+    // const mongoClient = await MongoClient.connect(connectionString);
+    // this.database = mongoClient.db(this.databaseName);
   }
 
 }
--- src/infrastructure/ApiServer.ts
+++ src/infrastructure/ApiServer.ts
@@ -1,7 +1,8 @@
 import * as restify from "restify";
-import { IApiController } from "./../app/apicontrollers/IApiController";
+import { IApiController } from "../app/api/IApiController";
 import { IInitializable } from "./IInitializable";
 import * as CorsMiddleware from "restify-cors-middleware2";
+import { connect } from "mongoose";
 
 export class ApiServer implements IInitializable {
 
@@ -11,20 +12,25 @@ export class ApiServer implements IInitializable {
 
   constructor(public apiControllers: IApiController[]) { }
 
-  public Initialize(): Promise<any> {
+  public async Initialize(): Promise<any> {
+    const connectionString: string = process.env.MONGO_URL || "";
+    console.log(connectionString);
+    await connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
     this.restifyServer = restify.createServer();
+
+    this.restifyServer.use(restify.plugins.queryParser());
+    this.restifyServer.use(restify.plugins.bodyParser());
 
     const cors =  CorsMiddleware({
       preflightMaxAge: 5,
-      origins: ["*"],
+      origins: ["*", /^http?:\/\/localhost(:[\d]+)?$/],
       allowHeaders: ["*"],
       exposeHeaders: []
     });
 
     this.restifyServer.pre(cors.preflight);
     this.restifyServer.use(cors.actual);
-    this.restifyServer.use(restify.plugins.queryParser());
-    this.restifyServer.use(restify.plugins.bodyParser());
 
     this.apiControllers.forEach((apiController) => {
       if (this.restifyServer !== undefined) {
@@ -34,13 +40,13 @@ export class ApiServer implements IInitializable {
 
     console.log("rest server starting...");
 
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
       if (this.restifyServer !== undefined) {
         this.restifyServer.listen(process.env.API_PORT, () => {
           if (this.restifyServer !== undefined) {
             console.log("%s is online on %s", this.restifyServer.name, this.restifyServer.url);
           }
-          resolve(true);
+          resolve();
         });
       }
 
